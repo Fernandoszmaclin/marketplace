532c07aba712092132fc0ae92069be505f01c8fb
Fernando Sozo Marcolin <fermarcolin1@gmail.com>
2025-10-05 15:30:12 -0300

    fix(pi): corrige erro 404 da front page

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'core/contact.html')

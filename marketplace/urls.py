c3e3056733c28101ebc7fdd2ae91155938b81c81
Fernando Sozo Marcolin <fermarcolin1@gmail.com>
2025-10-05 19:11:44 -0300

    feat(head): adiciona head da front page

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', index, name='index'), # Front page
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
